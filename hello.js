8e4c4458a11b1f09ce93489acaf8860fc882d06e
paoloLanaro <lanaro.p@northeastern.edu>
2023-02-03 17:20:16 -0800

    Basic code for the infrastructure of the program

--- hello.js
+++ hello.js
@@ -1,1 +1,51 @@
-console.log("hello");
+/*
+Water bottle saving idea
+GUI that counts the amount of plastic you have saved from recycling water bottles –
+or how much plastic you saved from ounces / liters of water drank from a metal bottle / reusable bottle
+Level based on how much plastic you saved
+Level 1 - 1000grams → star
+Level 2 - 2000grams etc.
+ */
+const averagePlasticPerBottle = 20; //grams
+let bottles = 0;
+const levels = ["Beginner", "Droplet", "Puddle", "Apprentice", "Enjoyer", "Trainer", "Wizard", "Bender", "Virtuoso", "Turtle's Best Friend"];
+const levelThresholds = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
+
+//Level -1 (Turtle Kill;er)
+//Level 0 (Beginner)
+//Level 1 (Water Droplet)
+//Level 2 (Water Puddle)
+//Level 3 (Water Muncher)
+//Level 4 (Water Trainer)
+//Level 5 (Water )
+//Level 6 (Water )
+//Level 7 (Water )
+//Level 8 (Water )
+//Level 9 (Water Shark)
+//Level 10 (Water Wizard)
+//Level 11 (Aqua Philosopher)
+//Level 12 (Water Virtuoso)
+//Level 13 (Water Bender)
+//Level 14 (A Turtle's Best Friend)
+//Level 11(What color is your water bottle?)
+//
+
+function increaseBottles() { //on click event
+    bottles++;
+}
+
+function calculatePlasticSaved() { //average plastic saved per amount of bottles
+    return bottles * averagePlasticPerBottle;
+}
+
+function returnLevel() {
+    if (bottles < 2) {
+        return "Turtle Killer";
+    }
+    for (let i = levels.length; i >= 0; i--) {
+        if (bottles <= levelThresholds[i]) {
+            return levels[i];
+        }
+    }
+    return levels[levelThresholds.length - 1];
+}
